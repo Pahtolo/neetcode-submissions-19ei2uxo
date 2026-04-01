3f8d3ec0faa9629aa51e83cd389cec0da1dfdff8
Michael Collins <111788127+Pahtolo@users.noreply.github.com>
2026-04-01 12:53:02 -0400

    Add: top-k-elements-in-list - submission-9

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-9.py
@@ -0,0 +1,16 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+       count = Counter(nums)
+       return heapq.nlargest(k, count, count.get)
+            
+
+
+
+        
+
+            
+        
+
+        
+
+
